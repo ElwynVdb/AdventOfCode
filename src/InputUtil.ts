a43151d2b0933cacf49c3006949032fe6ee268b1
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-12-02 09:33:42 +0100

    Day 02

--- src/InputUtil.ts
+++ src/InputUtil.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
 
-const readInputForDay = (day: string): string => fs.readFileSync(`../inputs/${day}.txt`, { encoding: "utf-8" });
+const readInputForDay = (day: string): string => fs.readFileSync(`${__dirname}/inputs/${day}.txt`, { encoding: "utf-8" });
 
 export default readInputForDay;

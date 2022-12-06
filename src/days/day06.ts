a49efd79acdaf8d929f8c9d1f0b62df584c2ba75
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-12-06 09:04:36 +0100

    Clean

--- src/days/day06.ts
+++ src/days/day06.ts
@@ -3,19 +3,16 @@ import readInputForDay from "../InputUtil";
 const input: string[] = readInputForDay("06").split("");
 
 const main = (size: number) => {
-    let temp = "";
-
     for (let x = 0; x < input.length; x++) {
+        let temp = "";
+
         for (let y = 0; y <= (size - 1); y++)
             temp += input[x + y] || "";
-
 
         const set = [...new Set(temp.split(""))];
 
         if (set.length === size)
             return x + size;
-
-        temp = "";
     }
 
     return -1;

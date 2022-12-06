ed1daad20a585ee200595807cf87bdd29d3e8572
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-12-06 07:48:37 +0100

    Day 06

---
+++ src/days/day06.ts
@@ -0,0 +1,30 @@
+import readInputForDay from "../InputUtil";
+
+const input: string[] = readInputForDay("06").split("");
+
+const main = (size: number) => {
+    let temp = "";
+
+    for (let x = 0; x < input.length; x++) {
+        for (let y = 0; y <= (size - 1); y++) {
+            const atIndex = input[x + y];
+            temp += atIndex ? atIndex : "";
+        }
+
+        const set = [...new Set(temp.split(""))];
+
+        if (set.length === size)
+            return x + size;
+
+        temp = "";
+    }
+
+    return -1;
+}
+
+const partOne = () => main(4);
+
+const partTwo = () => main(14)
+
+console.log(partOne());
+console.log(partTwo());

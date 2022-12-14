846c831f8b1f973a0c48aa6e1ed11056f4f771ca
Elwyn Van der Borght <38124619+ElwynVdb@users.noreply.github.com>
2022-12-15 00:19:53 +0100

    Part One Cleanup

--- src/days/day11.ts
+++ src/days/day11.ts
@@ -9,25 +9,18 @@ interface Monkey {
     test: (result: number) => boolean;
     truthyReceiver: number;
     falsyReceiver: number;
+    inspected: number;
 }
 
 const monkeys: Monkey[] = [];
 const numberRegex = /[^\d.]/g;
 
 const deserialize = () => {
-
     let indexMonkeyLine = 0;
-    let currentMonkey: Partial<Monkey> = {};
+    let currentMonkey: Partial<Monkey> = { inspected: 0 };;
 
     for (let x = 0; x < input.length; x++) {
         const line = input[x];
-
-        if (line === "") {
-            indexMonkeyLine = 0;
-            monkeys.push(currentMonkey as Monkey);
-            currentMonkey = {};
-            continue;
-        }
 
         indexMonkeyLine++;
 
@@ -40,6 +33,7 @@ const deserialize = () => {
                 break;
             case 3:
                 const actions = line.split(" ").slice(6);
+
                 currentMonkey.operation = (old) => {
                     const applyValue = actions[1] === "old" ? old : parseInt(actions[1]);
 
@@ -63,18 +57,42 @@ const deserialize = () => {
                 currentMonkey.falsyReceiver = parseInt(line.replace(numberRegex, ""));
                 break;
         }
+
+        if (line === "" || x === input.length - 1) {
+            indexMonkeyLine = 0;
+            monkeys.push(currentMonkey as Monkey);
+            currentMonkey = { inspected: 0 };
+            continue;
+        }
     }
 }
 
 deserialize();
 
-const partOne = () => {
+const checkItems = (rounds: number, worried:boolean) => {
 
+    for (let x = 0; x < rounds; x++) {
+        monkeys.forEach(monkey => {
+            monkey.items.forEach(item => {
+                monkey.inspected++;
+                const newValue = monkey.operation(item);
+                const bored = worried ? Math.floor(newValue / 3) : newValue;
+                const monkeyToPass = monkey.test(bored) ? monkey.truthyReceiver : monkey.falsyReceiver;
+                monkeys[monkeyToPass].items.push(bored);
+            });
+
+            monkey.items = [];
+        });
+    }
+
+
+    return monkeys.map(m => m.inspected).sort((a, b) => b - a).filter((c, i) => i <= 1).reduce((a, b) => a * b);
 }
 
-const partTwo = () => {
 
-}
+const partOne = () => checkItems(20, true);
+
+const partTwo = () => checkItems(10000, false);
 
 
 console.log(partOne());

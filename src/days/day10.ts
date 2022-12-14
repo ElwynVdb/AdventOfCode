23b227a0deff3c11c4d04cfa12f66d20b272922d
Elwyn Van der Borght <38124619+ElwynVdb@users.noreply.github.com>
2022-12-14 21:08:19 +0100

    Day 10 | Part One

---
+++ src/days/day10.ts
@@ -0,0 +1,61 @@
+import readInputForDay from "../InputUtil";
+
+const input = readInputForDay("10").split("\r\n");
+
+const LENGTHS: { [key: string]: number } = {
+    noop: 1,
+    addx: 2
+}
+
+interface IAction {
+    cyclesPassed: number;
+    maxCycles: number;
+    toAdd: number;
+}
+const outPut: { [key: number]: number } = {};
+
+const simulateCPU = () => {
+    const actions = createActions();
+
+    let cycle = 0;
+    let xReg = 1;
+    while (actions.length > 0) {
+        outPut[cycle + 1] = xReg;
+        const action = actions[0];
+
+        action.cyclesPassed++;
+
+        if (action.cyclesPassed == action.maxCycles) {
+            xReg += action.toAdd;
+            actions.shift();
+        }
+
+        cycle++;
+    }
+}
+
+const createActions = (): IAction[] => {
+    return input.map(cmd => {
+        const split = cmd.split(" ");
+
+        return {
+            cyclesPassed: 0,
+            maxCycles: LENGTHS[split[0]],
+            toAdd: split[1] ? parseInt(split[1]) : 0
+        };
+    })
+}
+
+const partOne = () => {
+    simulateCPU();
+
+    const fetch = [20, 60, 100, 140, 180, 220];
+    console.log(fetch.map(v => outPut[v] * v).reduce((a, b) => a + b));
+}
+
+const partTwo = () => {
+
+}
+
+console.log(partOne());
+console.log(partTwo());
